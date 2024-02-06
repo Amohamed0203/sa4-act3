1eb3a50b94d3e140f0236f348ebf81a48f3aa888
Ahmed Mohamed <ahmed.moh0203@gmail.com>
2024-02-06 15:51:47 -0600

    Added hint functionality

--- guess_number.py
+++ guess_number.py
@@ -4,8 +4,12 @@ print("I'm thinking of a number...")
 guess = str(input("What number am I thinking of? "))
 
 while guess != number and guess != 'q':
-    print("Try again. I'm thinking of a number...")
-    guess = str(input("What number am I thinking of? "))  
+    if int(guess) > int(number):
+        print("Try lower. I'm thinking of a number...")
+        guess = str(input("What number am I thinking of? ")) 
+    else:
+        print("Try higher. I'm thinking of a number...")
+        guess = str(input("What number am I thinking of? ")) 
 
 if guess == 'q':
     print(f"Sorry! The number was {number}.")   
