47ef3d244169757a9cc8aa38d82788ddf06a05d8
Ahmed Mohamed <ahmed.moh0203@gmail.com>
2024-02-06 15:46:36 -0600

    Added retry and quit functionality

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,13 @@
-number = 10
+number = '10'
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = str(input("What number am I thinking of? "))
 
-if guess == number:
+while guess != number and guess != 'q':
+    print("Try again. I'm thinking of a number...")
+    guess = str(input("What number am I thinking of? "))  
+
+if guess == 'q':
+    print(f"Sorry! The number was {number}.")   
+elif guess == number:
     print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
