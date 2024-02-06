58f52e09e7d971ce3782f965b75334a763147d8a
Ahmed Mohamed <ahmed.moh0203@gmail.com>
2024-02-06 15:58:28 -0600

    Added cap functionality

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,16 @@
 number = '10'
+cap = 9
 
-print("I'm thinking of a number...")
+print("10 tries. I'm thinking of a number...")
 guess = str(input("What number am I thinking of? "))
 
-while guess != number and guess != 'q':
-    print("Try again. I'm thinking of a number...")
-    guess = str(input("What number am I thinking of? "))  
+while guess != number and guess != 'q' and cap != 0:
+    print(f"{cap} more tries. Try again. I'm thinking of a number...")
+    guess = str(input("What number am I thinking of? "))
+    cap -= 1
 
+if cap == 0:
+    print(f"Sorry! The number was {number}.")  
 if guess == 'q':
     print(f"Sorry! The number was {number}.")   
 elif guess == number:
